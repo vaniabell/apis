3f913572859748eecafd116faf97afd779318b17
vaniabell <108189253+vaniabell@users.noreply.github.com>
2022-09-17 11:55:06 -0500

    end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 console.log("Entro al main.js");
+
+const randomImage = () => {
+
+    console.log("entro a la funcion randomImage");
+
+    const URL="https://picsum.photos/200/300"
+
+
+    fetch(URL)
+        .then((data) => {
+            console.log(data);
+            document.getElementById('imgRandom').src = data.url;
+        })
+        .catch((error) => {
+            console.log("Ocurrio un error!");
+        });
+
+
+      
+
+}
+
+
+
